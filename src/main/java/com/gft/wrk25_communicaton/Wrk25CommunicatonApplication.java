2e75cd55b6e1b45729a63aee66a5814f0f5ab8f8
jums <jums@gft.com>
2025-05-13 15:35:36 +0200

    Primer commit

---
+++ src/main/java/com/gft/wrk25_communicaton/Wrk25CommunicatonApplication.java
@@ -0,0 +1,13 @@
+package com.gft.wrk25_communicaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wrk25CommunicatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wrk25CommunicatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gft/wrk25_communicaton/Wrk25CommunicatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.wrk25_communicaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wrk25CommunicatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
