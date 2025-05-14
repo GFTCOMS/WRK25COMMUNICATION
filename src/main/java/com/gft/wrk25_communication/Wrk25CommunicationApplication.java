9c91101276324b87ca31b557266e04bc5c72ac07
Ivan Plaus Hedrera <inph@gft.com>
2025-05-14 12:26:57 +0200

    updated jacoco version and added @Generated to main method

--- src/main/java/com/gft/wrk25_communication/Wrk25CommunicationApplication.java
+++ src/main/java/com/gft/wrk25_communication/Wrk25CommunicationApplication.java
@@ -1,13 +1,13 @@
 package com.gft.wrk25_communication;
 
-import javax.annotation.Generated;
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Generated("excluded")
 public class Wrk25CommunicationApplication {
 
+    @Generated
     public static void main(String[] args) {
         SpringApplication.run(Wrk25CommunicationApplication.class, args);
     }
