0f37b5a350b1a2bd76995e770c57acaa7b0371be
martinsanchez <mnnh@gft.com>
2025-05-19 13:34:43 +0200

    application properties configurado para el funcionamiento de rabbitmq

--- src/main/java/com/gft/wrk25_communication/communication/domain/notification/NotificationRepository.java
+++ src/main/java/com/gft/wrk25_communication/communication/domain/notification/NotificationRepository.java
@@ -5,6 +5,7 @@ import com.gft.wrk25_communication.communication.domain.UserId;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface NotificationRepository {
 
     List<Notification> findAllByUserId(UserId id);
