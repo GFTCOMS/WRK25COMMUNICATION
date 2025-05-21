acb6a0b1e58ac790b08e6cd30e6fc3c461eae414
Ivan Plaus Hedrera <inph@gft.com>
2025-05-21 08:56:04 +0200

    Added deletion of LowStockNotificationPublisherTest.java and removed an unused import

--- src/main/java/com/gft/wrk25_communication/communication/domain/notification/NotificationRepository.java
+++ src/main/java/com/gft/wrk25_communication/communication/domain/notification/NotificationRepository.java
@@ -3,7 +3,6 @@ package com.gft.wrk25_communication.communication.domain.notification;
 import com.gft.wrk25_communication.communication.domain.UserId;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface NotificationRepository {
 
