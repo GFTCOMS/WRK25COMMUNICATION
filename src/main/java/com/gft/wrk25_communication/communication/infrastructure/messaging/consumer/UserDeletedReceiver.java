371e12cef9e62a968c18113431003ad94943f9b3
jums <jums@gft.com>
2025-06-02 13:03:56 +0200

    añadido codigo para metricas de rabbit en UserDeletedReceiver

--- src/main/java/com/gft/wrk25_communication/communication/infrastructure/messaging/consumer/UserDeletedReceiver.java
+++ src/main/java/com/gft/wrk25_communication/communication/infrastructure/messaging/consumer/UserDeletedReceiver.java
@@ -2,6 +2,8 @@ package com.gft.wrk25_communication.communication.infrastructure.messaging.consu
 
 import com.gft.wrk25_communication.communication.application.NotificationDeleteByUserIdUseCase;
 import com.gft.wrk25_communication.communication.domain.UserId;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,13 +15,21 @@ import org.springframework.stereotype.Component;
 public class UserDeletedReceiver {
 
     private final NotificationDeleteByUserIdUseCase notificationDeleteByUserIdUseCase;
+    private final Tracer tracer;
 
     @RabbitListener(queues = "${queue.user.deleted}")
     public void receive(UserId userId) {
-        log.info("User deleted received : userId={}",
-                userId.userId());
+        Span span = tracer.nextSpan().name("process.user-deleted-message").start();
+        try (Tracer.SpanInScope scope = tracer.withSpan(span)) {
+            log.info("User deleted received : userId={}", userId.userId());
 
-        notificationDeleteByUserIdUseCase.execute(userId);
+            notificationDeleteByUserIdUseCase.execute(userId);
+
+        } catch (Exception e) {
+            span.error(e);
+            throw e;
+        } finally {
+            span.end();
+        }
     }
-
 }
