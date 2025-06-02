b27f29b65ffab9dc12f922b2ed217cb2cf33f3c7
Ivan Plaus Hedrera <inph@gft.com>
2025-06-02 11:11:08 +0200

    Creado el receiver para recibir el cambio de un producto desde carrito

---
+++ src/main/java/com/gft/wrk25_communication/communication/infrastructure/messaging/consumer/ProductInCartChangedNotificationReceiver.java
@@ -0,0 +1,50 @@
+package com.gft.wrk25_communication.communication.infrastructure.messaging.consumer;
+
+import com.gft.wrk25_communication.communication.application.NotificationSaveUseCase;
+import com.gft.wrk25_communication.communication.application.dto.CartProductChangedDTO;
+import com.gft.wrk25_communication.communication.application.dto.ProductDTO;
+import com.gft.wrk25_communication.communication.application.dto.ProductStockChangedNotificationDTO;
+import com.gft.wrk25_communication.communication.application.web.ApiClient;
+import com.gft.wrk25_communication.communication.domain.ProductId;
+import com.gft.wrk25_communication.communication.domain.UserId;
+import com.gft.wrk25_communication.communication.domain.notification.Notification;
+import com.gft.wrk25_communication.communication.domain.notification.NotificationFactory;
+import com.gft.wrk25_communication.communication.infrastructure.exception.ProductNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductInCartChangedNotificationReceiver {
+
+    private final ApiClient apiClient;
+    private final NotificationFactory notificationFactory;
+    private final NotificationSaveUseCase notificationSaveUseCase;
+
+    @RabbitListener(queues = "${queue.cart.product.changed}")
+    public void receive(CartProductChangedDTO notification) {
+
+        log.info("CartProductChanged notification received: userId={}, productId={}",
+                notification.productId(), notification.userId());
+
+        UserId userId = new UserId(notification.userId());
+
+        ProductId productId = new ProductId(notification.productId());
+
+        ProductDTO product = apiClient.getProductById(productId);
+
+        if (product == null) {
+            throw new ProductNotFoundException(productId);
+        }
+
+        Notification notificationToSave = notificationFactory.createProductChangedNotification(userId, product);
+
+        notificationSaveUseCase.execute(notificationToSave);
+    }
+
+}
---
+++ src/test/java/com/gft/wrk25_communication/communication/infrastructure/messaging/consumer/ProductInCartChangedNotificationReceiverTest.java
@@ -0,0 +1,83 @@
+package com.gft.wrk25_communication.communication.infrastructure.messaging.consumer;
+
+import com.gft.wrk25_communication.communication.application.NotificationSaveUseCase;
+import com.gft.wrk25_communication.communication.application.dto.CartProductChangedDTO;
+import com.gft.wrk25_communication.communication.application.dto.ProductDTO;
+import com.gft.wrk25_communication.communication.application.dto.ProductStockChangedNotificationDTO;
+import com.gft.wrk25_communication.communication.application.web.ApiClient;
+import com.gft.wrk25_communication.communication.domain.ProductId;
+import com.gft.wrk25_communication.communication.domain.UserId;
+import com.gft.wrk25_communication.communication.domain.notification.Notification;
+import com.gft.wrk25_communication.communication.domain.notification.NotificationFactory;
+import com.gft.wrk25_communication.communication.infrastructure.exception.ProductNotFoundException;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductInCartChangedNotificationReceiverTest {
+
+    @Mock
+    ApiClient apiClient;
+
+    @Mock
+    NotificationFactory notificationFactory;
+
+    @Mock
+    NotificationSaveUseCase notificationSaveUseCase;
+
+    @InjectMocks
+    private ProductInCartChangedNotificationReceiver receiver;
+
+    @Test
+    void testReceive() {
+
+        UserId userId = new UserId(UUID.randomUUID());
+        ProductDTO product = Instancio.create(ProductDTO.class);
+        ProductId productId = new ProductId(product.id());
+
+        CartProductChangedDTO notification = new CartProductChangedDTO(
+                userId.userId(),
+                product.id()
+        );
+
+        Notification notificationToReturn = Instancio.create(Notification.class);
+
+        when(apiClient.getProductById(productId)).thenReturn(product);
+
+        when(notificationFactory.createProductChangedNotification(userId, product)).thenReturn(notificationToReturn);
+
+        receiver.receive(notification);
+
+        verify(apiClient, times(1)).getProductById(productId);
+        verify(notificationFactory, times(1)).createProductChangedNotification(userId, product);
+        verify(notificationSaveUseCase,times(1)).execute(notificationToReturn);
+    }
+
+    @Test
+    void testReceiveProductIdNotFound() {
+
+        UserId userId = new UserId(UUID.randomUUID());
+        ProductDTO product = Instancio.create(ProductDTO.class);
+        ProductId productId = new ProductId(product.id());
+
+        CartProductChangedDTO notification = new CartProductChangedDTO(
+                userId.userId(),
+                product.id()
+        );
+
+        when(apiClient.getProductById(productId)).thenReturn(null);
+
+        assertThrows(ProductNotFoundException.class, () -> receiver.receive(notification));
+    }
+
+}
