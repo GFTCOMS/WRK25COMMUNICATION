67347927e1979ef4f7da83e6b9f4b669ffcb4426
Ivan Plaus Hedrera <inph@gft.com>
2025-06-02 12:48:21 +0200

    Cambiado el atibuto id

--- src/main/java/com/gft/wrk25_communication/communication/infrastructure/messaging/consumer/UserDeletedReceiver.java
+++ src/main/java/com/gft/wrk25_communication/communication/infrastructure/messaging/consumer/UserDeletedReceiver.java
@@ -1,6 +1,7 @@
 package com.gft.wrk25_communication.communication.infrastructure.messaging.consumer;
 
 import com.gft.wrk25_communication.communication.application.NotificationDeleteByUserIdUseCase;
+import com.gft.wrk25_communication.communication.application.dto.UserDeletedDTO;
 import com.gft.wrk25_communication.communication.domain.UserId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +16,11 @@ public class UserDeletedReceiver {
     private final NotificationDeleteByUserIdUseCase notificationDeleteByUserIdUseCase;
 
     @RabbitListener(queues = "${queue.user.deleted}")
-    public void receive(UserId userId) {
-        log.info("User deleted received : userId={}",
-                userId.userId());
+    public void receive(UserDeletedDTO userId) {
+        log.info("UserDeletedDTO received : userId={}",
+                userId.id());
 
-        notificationDeleteByUserIdUseCase.execute(userId);
+        notificationDeleteByUserIdUseCase.execute(new UserId(userId.id()));
     }
 
 }
